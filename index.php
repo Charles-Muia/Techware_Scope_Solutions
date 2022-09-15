a3f109ba0aad6af110b1a4bff59d9ef37a6bda57
Charles-Muia <Charles.K.Muia@gmail.com>
2022-09-15 14:04:31 +0300

    Techware_Scope_Solutions

---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <title> Techware Scope Solutions | Home </title>
+</head>
+<body>
+                <!--Navigation -->
+
+<section id="navigation">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="Photos/Log_2.jpg" alt="" height="80"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <hr>
+</section>
+
+                <!--Carousel section for photos-->
+
+            <div id="arouse_photos">
+                <div id="arousel_cators" class="carousel slide" data-bs-ride="true">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#arousel_cators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#arousel_cators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#arousel_cators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="Photos/Our_Services_4.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Photos/Untitled_4.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Photos/logo_3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#arousel_cators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#arousel_cators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+
+                <!--About section-->
+
+            <section id="about_site">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                    <h2> About Us</h2>
+                                        <div class="about_stuff">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur, facere laborum
+                                            neque nostrum numquam odit officiis porro quasi sequi! Aliquid aut autem cumque
+                                            earum fugit magni quaerat quam quis.
+                                            <br><br>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet at delectus deleniti
+                                            eum incidunt perferendis saepe.
+                                            At dignissimos esse eum itaque nulla! Id laudantium perferendis quam qui quidem tempora?
+                                        </div>
+                                                <button type="button" class="btn btn-primary"> Read More ->>></button>
+                            </div>
+                                 <div class="col-md-6 para-bar">
+                                                <p> Wed Design & Hosting </p>
+                                                <div class="progress">
+                                                    <div class="progress-bar" style="width: 90%;"> 90% </div>
+                                                </div>
+                                               <p> Networking & Telephony </p>
+                                               <div class="progress">
+                                                    <div class="progress-bar" style="width: 90%;"> 70% </div>
+                                               </div>
+                                                <p> ICT Support & Maintenance</p>
+                                                <div class="progress">
+                                                    <div class="progress-bar" style="width: 100%;"> 100% </div>
+                                                </div>
+                                                <p> Office Equipment & Stationery </p>
+                                                <div class="progress">
+                                                    <div class="progress-bar" style="width: 80%;"> 65% </div>
+                                                </div>
+                                 </div>
+                            </div>
+                        </div>
+            </section>
+
+                <!--Services section-->
+
+            <section id="serv_sec">
+                        <div class="container">
+                            <h2>Our Offering</h2>
+
+                        </div>
+            </section>
+
+
+</body>
+</html>
+

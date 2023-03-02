9eb08533ed1c6fcf232e9d49de1cf8438f27d4c9
Charles-Muia <Charles.K.Muia@gmail.com>
2023-03-02 16:38:42 +0300

    Techware_Scope_Solutions

---
+++ epson_projectors.php
@@ -0,0 +1,310 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <title> Techware Scope Solutions | Blog </title>
+    <link rel="icon" type="" href="Photos/Company_Logo.jpg">
+</head>
+<body>
+
+<!------------------------------------------------Navigation section------------------------------------------------->
+
+
+<section id="navigation">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="Photos/Log_2.jpg" alt="" height="80"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=".#about_site">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=".#serv_sec">Our Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=".#our_blog">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=".#FAQs_sec">FAQs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=".#cont_us">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
+
+<!--------------------------------------------------Blog page content------------------------------------------------>
+
+<!--<section class="blog-content">-->
+<!--    <div class="blog-content-row">-->
+<!--        <div class="blog-content-left">-->
+<!--            <img src="Photos/Windows11.png" alt="">-->
+<!--            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, optio, sapiente.</h2>-->
+<!--                    <p>-->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!---->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!--                    </p>-->
+<!---->
+<!--                    <p>-->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!---->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!--                    </p>-->
+<!--                    <p>-->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!---->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!--                    </p>-->
+<!---->
+<!--                    <p>-->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!---->
+<!--                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.-->
+<!--                        Asperiores laboriosam mollitia natus soluta totam voluptates!-->
+<!--                        A architecto eveniet magni minima molestias mollitia neque nostrum,-->
+<!--                        obcaecati quam velit! Atque, delectus, deleniti.-->
+<!--                    </p>-->
+<!---->
+<!--                    <div class="comment-box">-->
+<!--                        <h3>Leave a comment</h3>-->
+<!--                        <form class="comment-form" action="">-->
+<!--                            <input type="text" placeholder="Enter your name">-->
+<!--                            <input type="email" placeholder="Enter your email address">-->
+<!--                            <textarea rows="10" placeholder="Write your comment here"></textarea>-->
+<!--                            <button type="submit" class="comment-btn"> Post Comment </button>-->
+<!--                        </form>-->
+<!--                    </div>-->
+<!--        </div>-->
+<!---->
+<!--<!--        <div class="blog-content-right">-->
+<!--<!--            <h3>Lorem ipsum dolor sit amet.</h3>-->
+<!--<!--                <div>-->
+<!--<!--                    <span> <img src="Photos/Windows11.png" alt=""> </span>-->
+<!--<!--                </div>-->
+<!---->
+<!--        </div>-->
+<!--    </div>-->
+<!--</section>-->
+
+<!-----------------------------------------------Blog page post content---------------------------------------------->
+<section id="blog-post">
+    <div class="blogs">
+        <div class="post">
+            <img src="Photos/Windows11.png" alt="">
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quos?</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci amet commodi consequatur corporis deserunt earum error est,
+                exercitationem id incidunt iusto maiores quasi rem repellat sed,
+                temporibus, voluptates. Explicabo, qui.
+            </p>
+        </div>
+    </div>
+</section>
+
+
+
+<!--------------------------------------------------Footer section--------------------------------------------------->
+
+<footer>
+    <section class="foot_sec">
+        <div class="container">
+            <div class="foot_abt">
+                <h5> About Us </h5>
+                <p> <span id="rd">Tech</span>ware Scope Solutions offers ICT, office equipment, stationery supply & digital consultancy services
+                    delivered by a highly skilled team with expertise in:<i class="bi bi-dash-lg"></i>
+                    <br><br>
+                    <!--                            praesentium quaerat recusandae saepe sed velit voluptates!-->
+                    <!--                            Aperiam architecto iure molestiae quo velit.-->
+                    <!--                            <br><br>-->
+                    IT, social media (digital communication), information security, networking, technical support, web design and hosting.
+                    <!--                            Our business is supporting yours.-->
+                </p>
+
+                <ul class="lst_inks">
+                    <!--                                        <li><a href="#"><i class="bi bi-facebook"></i></a></li>-->
+                    <li><a href="https://www.facebook.com/TechwareScopeSolutions"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/S_TechwareScope"><i class="bi bi-twitter"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/techware-scope-solutions/"><i class="bi bi-linkedin"></i></a></li>
+                    <li><a href="#"><i class="bi bi-whatsapp"></i></i></a></li>
+                    <li><a href="#"><i class="bi bi-youtube"></i></a></li>
+                    <li><a href="#"><i class="bi bi-github"></i></a></li>
+                </ul>
+            </div>
+            <div class="foot_links">
+                <h5> Quick Links </h5>
+                <ul>
+                    <li><a href="#"> Home </a></li>
+                    <li><a href=".#about_site"> About Us </a></li>
+                    <li><a href=".#serv_sec"> Our Services </a></li>
+                    <li><a href=".#our_services"> Solutions </a></li>
+                    <li><a href=".#port_sec"> Portfolio </a></li>
+                    <li><a href=".#our_blog"> Blog </a></li>
+                    <li><a href=".#FAQs_sec"> FAQs </a></li>
+                    <li><a href=".#cont_us"> Contact Us </a></li>
+                </ul>
+            </div>
+
+            <div class="foot_cont">
+                <h5> Contact Us </h5>
+                <ul class="find_info">
+                    <li>
+                        <span> <i class="bi bi-geo-alt-fill"></i> </span>
+                        <span> Ghana Street <br> Opposite Maguna Downtown (CBD)
+                                        <br> Meru County </span>
+                    </li>
+                    <li>
+                        <span> <i class="bi bi-telephone-fill"></i> </span>
+                        <p>
+                            <a href="+254 7333 7333"> +254 724 563 163 </a> <br>
+                            <a href="+254 7333 7333"> +254 784 563 163 </a>
+                        </p>
+                    </li>
+                    <li>
+                        <span> <i class="bi bi-send-fill"></i> </span>
+                        <p>
+                            <a href="info.techwarescopesolutions@gmail.com"> info.techwarescopesolutions@gmail.com </a>
+                        </p>
+                    </li>
+                    <li>
+                        <span><i class="bi bi-clock-fill"></i></span>
+                        <p>
+                            <a href="Mon - Sat 8:00 AM - 5:00PM"> Mon - Sat 8:00 AM - 5:00PM <br> Closed: Sun & Public / Bank Holidays </a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--                <div class="foot_copy">-->
+        <!--                    <p>-->
+        <!--                        Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2022. <span>Tech</span>ware Scope Solutions. All Rights Reserved.-->
+        <!--                    </p>-->
+        <!---->
+        <!--                </div>-->
+    </section>
+    <div class="foot_copy">
+        <p>
+            Copyright <i class="fa fa-copyright" aria-hidden="true"></i>  2023. <span>Tech</span>ware Scope Solutions. All Rights Reserved.
+        </p>
+
+    </div>
+</footer>
+
+<!-----------------------------------------------Script (smooth-scroll)---------------------------------------------->
+
+
+<script src="js/smooth-scroll.js"></script>
+
+<script>
+    var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+
+<!--            <div class="foot_copy">-->
+<!--                <p>-->
+<!--                    Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2022. <span>Tech</span>ware Scope Solutions. All Rights Reserved.-->
+<!--                </p>-->
+<!---->
+<!--            </div>-->
+
+<!----------------------------------------------script for FAQs section---------------------------------------------->
+
+<script>
+    const faqs = document.querySelectorAll(".faq");
+
+    faqs.forEach((faq) => {
+        faq.addEventListener("click", () => {
+            faq.classList.toggle("active");
+        });
+    });
+
+</script>
+
+</body>
+</html>
+
+
+

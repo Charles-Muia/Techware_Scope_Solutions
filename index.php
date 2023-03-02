8422822f8eaac13a3219ca211c54274dfd7e8cb1
Charles-Muia <Charles.K.Muia@gmail.com>
2023-03-02 16:31:36 +0300

    Techware_Scope_Solutions

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
               <section id="navigation">
                 <nav class="navbar navbar-expand-lg bg-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href=""><img src="Photos/Log_2.jpg" alt="" height="80"></a>
+                        <a class="navbar-brand" href="index.php"><img src="Photos/Log_2.jpg" alt="" height="80"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>

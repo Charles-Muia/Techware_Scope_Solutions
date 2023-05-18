7f5c232e35803155c576acea366ffa1935ce6c98
Charles-Muia <Charles.K.Muia@gmail.com>
2023-05-18 19:05:06 +0300

    Techware Scope Solutions

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
               <section id="navigation">
                 <nav class="navbar navbar-expand-lg bg-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#"><img src="Photos/Log_2.jpg" alt="" height="80"></a>
+                        <a class="navbar-brand" href=""><img src="Photos/Log_2.jpg" alt="" height="80"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -881,6 +881,36 @@
                         });
                     });
 
+                </script>
+
+<!-------------------------------------------JS script for Smooth Scroll------------------------------------------------>
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script>
+                    $(document).ready(function(){
+                        // Add smooth scrolling to all links
+                        $("a").on('click', function(event) {
+
+                            // Make sure this.hash has a value before overriding default behavior
+                            if (this.hash !== "") {
+                                // Prevent default anchor click behavior
+                                event.preventDefault();
+
+                                // Store hash
+                                var hash = this.hash;
+
+                                // Using jQuery's animate() method to add smooth page scroll
+                                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                                $('html, body').animate({
+                                    scrollTop: $(hash).offset().top
+                                }, 2500, function(){
+
+                                    // Add hash (#) to URL when done scrolling (default click behavior)
+                                    window.location.hash = hash;
+                                });
+                            } // End if
+                        });
+                    });
                 </script>
 
 </body>
